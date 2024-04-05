816ca06a02f4dc123a43b0e91b04c072d2caba4e
LeonW <leon_1607@hotmail.de>
2024-04-05 23:57:39 +0200

    added version number to console title

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main()
     {
+        string version = "0.1.3-beta";
+
+        Console.Title = $"CrewChief_AutoStart - v{version}";
+
         GameManager.Start();
     }
 }
